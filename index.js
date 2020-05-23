098754fa882b2791d63e78e56d3c200f86e1541f
Adrian Mullings <adralemechk@gmail.com>
2020-05-23 14:00:15 -0400

    Only change roles as necessary, instead of clearing every time

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ const { GraphQLClient } = require('graphql-request');
 const fetch = require('node-fetch');
 const similarity = require('string-similarity').compareTwoStrings;
 
+/** @typedef {import('discord.js').Client} DiscordClient */
+/** @typedef {import('discord.js').Guild} Guild */
+/** @typedef {import('discord.js').GuildMember} GuildMember */
+/** @typedef {import('discord.js').Role} Role */
+/** @typedef {import('discord.js').Snowflake} Snowflake */
+
+/**
+ * @typedef {Object} Player
+ * @property {GuildMember} member
+ * @property {Snowflake[]} roles
+ * @property {string} handle
+ * @property {string} prefix
+ */
+
 const EVENTS_QUERY = `
 query EventsQuery($slug: String) {
   tournament(slug: $slug) {
@@ -60,11 +74,11 @@ async function main() {
   const roles = await getRoles(guild,  Object.keys(config.roles));
   const members = await getMembers(guild);
 
-  await removeRolesFromMembers(members, roles);
-
   const players = await getPlayersAndRoles(graphqlClient, tournamentSlug, members, config.roles);
 
-  await Promise.all(players.map(addRoles));
+  await removeRolesForNonPlayers(roles, members, players);
+
+  await Promise.all(players.map(p => setRoles(roles, p)));
 
   await Promise.all(players.map(setNickname));
 
@@ -78,6 +92,11 @@ async function getDiscordClient(credentials) {
   return discordClient;
 }
 
+/**
+ * @param {DiscordClient} discordClient
+ * @param {Snowflake} serverId
+ * @returns {Guild}
+ */
 function getGuild(discordClient, serverId) {
   const guild = discordClient.guilds.resolve(serverId);
   if (!guild) {
@@ -88,6 +107,11 @@ function getGuild(discordClient, serverId) {
   return guild;
 }
 
+/**
+ * @param {Guild} guild
+ * @param {Snowflake[]} roleIds
+ * @returns {Promise<Role[]>}
+ */
 async function getRoles(guild, roleIds) {
   const roles = Array.from((await guild.roles.fetch()).cache.values())
     .filter(role => roleIds.includes(role.id));
@@ -98,6 +122,10 @@ async function getRoles(guild, roleIds) {
   return roles;
 }
 
+/**
+ * @param {Guild} guild
+ * @returns {Promise<GuildMember[]>}
+ */
 async function getMembers(guild) {
   return Array.from((await guild.members.fetch()).values());
 }
@@ -109,6 +137,9 @@ async function removeRolesFromMembers(members, roles) {
   await Promise.all(membersWithRoles.map(m => m.roles.remove(roleIds)));
 }
 
+/**
+ * @returns {Promise<Player[]>}
+ */
 async function getPlayersAndRoles(graphqlClient, tournamentSlug, members, rolesConfig) {
   const playersByDiscordId = {};
   const events = await getEvents(graphqlClient, tournamentSlug);
@@ -119,7 +150,7 @@ async function getPlayersAndRoles(graphqlClient, tournamentSlug, members, rolesC
         `No corresponding role found for ${event.videogame.name} (ID: ${event.videogame.id})`);
       continue;
     }
-    
+
     const participants = await getEventParticipants(graphqlClient, event.id)
     for (const participant of participants) {
       const member = getMemberForParticipant(members, participant);
@@ -148,9 +179,12 @@ async function getEvents(graphqlClient, tournamentSlug) {
   return data.tournament.events;
 }
 
+/**
+ * @returns {Snowflake[]}
+ */
 function getRolesIdsForVideogame(videogame, rolesConfig) {
   return Object.entries(rolesConfig)
-    .filter(([ _, gameIdOrName ]) => 
+    .filter(([ _, gameIdOrName ]) =>
       gameIdOrName == videogame.id || gameIdOrName == videogame.name)
     .map(([ roleId, _ ]) => roleId);
 }
@@ -192,9 +226,45 @@ function getMemberForParticipant(members, participant) {
   return member;
 }
 
-async function addRoles(player) {
-  console.log(`Adding role(s) ${player.roles} to ${player.member.displayName}`)
-  await player.member.roles.add(player.roles);
+/**
+ * @param {Role[]} managedRoles
+ * @param {GuildMember[]} members
+ * @param {Player[]} players
+ * @returns {Promise<GuildMember[]>}
+ */
+async function removeRolesForNonPlayers(managedRoles, members, players) {
+  const tournamentMemberIds = new Set(players.map(p => p.member.id));
+  const roleIds = new Set(managedRoles.map(r => r.id));
+  return Promise.all(
+    members.filter(m => !tournamentMemberIds.has(m.id))
+      .map(m => {
+        if (Array.from(m.roles.cache.keys()).some(id => roleIds.has(id))) {
+          console.log(`Removing role(s) ${managedRoles.map(r => r.name)} from ${m.displayName}`)
+          return m.roles.remove(Array.from(roleIds));
+        }
+      })
+  );
+}
+
+/**
+ * @param {Role[]} managedRoles
+ * @param {Player} player
+ */
+async function setRoles(managedRoles, player) {
+  const current = Array.from(player.member.roles.cache.values()).map(v => v.id);
+  const toAdd = player.roles.filter(id => !current.includes(id));
+  const toRemove = managedRoles.map(r => r.id).filter(
+    id => current.includes(id) && !player.roles.includes(id));
+  /** @type {function(Snowflake[]): string[]} */
+  const getNames = roleIds => roleIds.map(id => managedRoles.find(r => r.id == id).name);
+  if (toAdd.length) {
+    console.log(`Adding role(s) ${getNames(toAdd)} to ${player.member.displayName}`)
+    await player.member.roles.add(toAdd);
+  }
+  if (toRemove.length) {
+    console.log(`Removing role(s) ${getNames(toRemove)} from ${player.member.displayName}`)
+    await player.member.roles.remove(toRemove);
+  }
 }
 
 async function setNickname(player) {
